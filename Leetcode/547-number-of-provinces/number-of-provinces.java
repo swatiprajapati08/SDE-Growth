74b996ca27343ef3802323f1f16d8e8ddcc6a302
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2025-05-22 23:40:13 +0530

    Time: 1 ms (88.57%) | Memory: 48.6 MB (5.12%) - LeetSync

--- Leetcode/547-number-of-provinces/number-of-provinces.java
+++ Leetcode/547-number-of-provinces/number-of-provinces.java
@@ -3,17 +3,35 @@ class Solution {
         int n = isConnected.length;
         boolean visited[] = new boolean[n];
         int count = 0;
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                DFS(i,visited,isConnected); count++;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                DFS(i, visited, isConnected);
+                count++;
             }
         }
         return count;
     }
-    void DFS(int src, boolean[] visited, int[][] arr){
+
+    void DFS(int src, boolean[] visited, int[][] arr) {
         visited[src] = true;
-        for(int i=0;i<arr.length;i++){
-            if(!visited[i] && arr[src][i] == 1)DFS(i,visited,arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (!visited[i] && arr[src][i] == 1)
+                BFS(i, visited, arr);
+        }
+    }
+
+    void BFS(int src, boolean[] visited, int[][] arr) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(src);
+        visited[src] = true;
+        while (!queue.isEmpty()) {
+            int p = queue.poll();
+            for (int i = 0; i < arr.length; i++) {
+                if (!visited[i] && arr[p][i] == 1) {
+                    queue.add(i);
+                    visited[i] = true;
+                }
+            }
         }
     }
 }
