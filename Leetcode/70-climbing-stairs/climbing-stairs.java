ef1b191c555c7faef277198acf1aae6b5725be42
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2026-04-06 22:27:12 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (62.53%) - LeetSync

--- Leetcode/70-climbing-stairs/climbing-stairs.java
+++ Leetcode/70-climbing-stairs/climbing-stairs.java
@@ -1,25 +1,33 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
-        Arrays.fill(dp,0);
-        return climbStairs(n,new int[n+1]);
+        int dp[] = new int[n + 1];
+        // Arrays.fill(dp, 0);
+        // return climbStairs(n, new int[n + 1]);
+
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+
+        return dp[n];
     }
 
-    int climbStairs(int n,int[] dp){
-        if(n == 0) return 1;
+    // using recurision
+    int climbStairs(int n, int[] dp) {
+        if (n == 0)
+            return 1;
 
-        if(n < 0 ) return 0;
+        if (n < 0)
+            return 0;
 
         // use dp
-        if(dp[n] != 0) return dp[n];
+        if (dp[n] != 0)
+            return dp[n];
 
+        int step1 = climbStairs(n - 1, dp);
+        int step2 = climbStairs(n - 2, dp);
 
-        int step1 = climbStairs(n-1,dp);
-        int step2 = climbStairs(n-2,dp);
+        return dp[n] = step1 + step2;
 
-
-        return dp[n] = step1+step2;
-
-        
     }
 }
